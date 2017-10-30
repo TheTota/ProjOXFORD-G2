ca1c588847c1780c247fe70f2a6395ca2a867767
thomasl <thomaslaure3@gmail.com>
2017-10-30 16:53:07 +0100

    Implémentation des méthodes de connexion à la base de données MySQL.

--- ProjOXFORD-G2/RecupIdentifiants.cs
+++ ProjOXFORD-G2/RecupIdentifiants.cs
@@ -10,11 +10,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;  // Librairie de connexion à MySQL ajoutée en référence.
 
-// Librairie de connexion à MySQL ajoutée en référence.
-using MySql.Data.MySqlClient;
-
-namespace ProjOXFORD_G2
+namespace ProjOXFORDG2
 {
     /// <summary>
     /// Classe qui va permettre la récupération des identifiants liés à la photo prise.
@@ -23,23 +22,69 @@ namespace ProjOXFORD_G2
     {
         /// <summary>
         /// Membre privé contenant les informations de connexion à la base de données.
+        /// Le @ sert à prendre la chaîne de caractères telle quelle.
         /// </summary>
-        private const string CNX = "database=testOxford; server=localhost; user id=root; pwd=";
+        private const string CNX = @"Server=127.0.0.1; Port=3306; Database=oxford; Uid=root; Pwd=;";
+
+        /// <summary>
+        /// Membre privé qui va contenir la requête.
+        /// </summary>
+        private string _requete;
+
+        /// <summary>
+        /// Déclaration d'un objet de la classe MysqlConnection.
+        /// Va être utilisé pour gérer la connexion à la base de données MySQL.
+        /// </summary>
+        private static MySqlConnection _connexion;
 
         /// <summary>
         /// Méthode de connexion à la base de données.
         /// </summary>
-        public static void MysqlConnect()
+        public static void OuvrirConnexion()
         {
-            MySqlConnection connexion = new MySqlConnection(CNX);
-
             try
             {
-                connexion.Open();
+                _connexion = new MySqlConnection(CNX);
+                if (_connexion.State == ConnectionState.Closed)
+                {
+                    _connexion.Open();
+                }
             }
-            catch (MySqlException)
+            catch
             {
-                throw;
+                throw new Exception("Erreur de connexion à la base de données MySQL.");
+            }
+        }
+
+        /// <summary>
+        /// Méthode publique de fermeture de la connexion à la base de données MySQL.
+        /// </summary>
+        public static void FermerConnexion()
+        {
+            if (_connexion.State != ConnectionState.Closed)
+            {
+                _connexion.Dispose();
+                _connexion.Close();
+            }
+        }
+
+        public void Comparer()
+        {
+
+        }
+
+        public void RecupInfosUser()
+        {
+            this._requete = @"SELECT photo, nom, prenom, type, birth, sexe, status, email FROM users INNER JOIN photos ON users.photo = photos.id WHERE faceid = @faceid;";
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(this._requete, _connexion);
+                cmd.CommandType = CommandType.Text;
+                Console.WriteLine("Requête effectuée.");
+            }
+            catch
+            {
+                throw new Exception("La requête n'a pu aboutir.");
             }
         }
     }
