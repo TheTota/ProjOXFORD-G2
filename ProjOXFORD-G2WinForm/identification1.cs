76c798bfb2b00a6b007dbc86bfb803bd9d2f2ce4
Loïc DELAUNAY <loicdelaunay05@gmail.com>
2017-11-06 14:30:24 +0100

    Preparation àllah mise en place du winform

---
+++ ProjOXFORD-G2WinForm/identification1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjOXFORD_G2WinForm
+{
+    public partial class identification1 : MetroFramework.Forms.MetroForm
+    {
+        public identification1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
