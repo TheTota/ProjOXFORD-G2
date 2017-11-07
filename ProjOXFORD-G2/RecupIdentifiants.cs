26500e8c29b180832091d65bffa3a783d296fe73
loïc delaunay <loicdelaunay05@gmail.com>
2017-11-07 19:31:33 +0100

    Correction du bug sur recupId à la volé

--- ProjOXFORD-G2/RecupIdentifiants.cs
+++ ProjOXFORD-G2/RecupIdentifiants.cs
@@ -1,101 +1,110 @@
-﻿////-----------------------------------------------------------------------
-//// <copyright file="RecupIdentifiants.cs" company="SIO">
-////     Copyright (c) SIO. All rights reserved.
-//// </copyright>
-//// <author>Thomas LAURE</author>
-////-----------------------------------------------------------------------
+﻿//-----------------------------------------------------------------------
+// <copyright file="RecupIdentifiants.cs" company="SIO">
+//     Copyright (c) SIO. All rights reserved.
+// </copyright>
+// <author>Thomas LAURE</author>
+//-----------------------------------------------------------------------
 
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using System.Data;
-//using MySql.Data.MySqlClient;  // Librairie de connexion à MySQL ajoutée en référence.
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;  // Librairie de connexion à MySQL ajoutée en référence.
 
-//namespace ProjOXFORDG2
-//{
-//    /// <summary>
-//    /// Classe qui va permettre la récupération des identifiants liés à la photo prise.
-//    /// </summary>
-//    public class RecupIdentifiants
-//    {
-//        /// <summary>
-//        /// Membre privé contenant les informations de connexion à la base de données.
-//        /// Le @ sert à prendre la chaîne de caractères telle quelle.
-//        /// </summary>
-//        private const string CNX = @"Server=127.0.0.1; Port=3306; Database=oxford; Uid=root; Pwd=;";
+namespace ProjOXFORD_G2
+{
+    /// <summary>
+    /// Classe qui va permettre la récupération des identifiants liés à la photo prise.
+    /// </summary>
+    public class RecupIdentifiants
+    {
+        /// <summary>
+        /// Membre privé contenant les informations de connexion à la base de données.
+        /// Le @ sert à prendre la chaîne de caractères telle quelle.
+        /// </summary>
+        private const string CNX = @"Server=127.0.0.1; Port=3306; Database=oxford; Uid=root; Pwd=;";
 
-//        /// <summary>
-//        /// Membre privé qui va contenir la requête.
-//        /// </summary>
-//        private string _requete;
+        /// <summary>
+        /// Déclaration d'un objet de la classe MysqlConnection.
+        /// Va être utilisé pour gérer la connexion à la base de données MySQL.
+        /// </summary>
+        private static MySqlConnection _connexion;
 
-//        /// <summary>
-//        /// Déclaration d'un objet de la classe MysqlConnection.
-//        /// Va être utilisé pour gérer la connexion à la base de données MySQL.
-//        /// </summary>
-//        private static MySqlConnection _connexion;
+        /// <summary>
+        /// Membre privé qui va contenir la requête.
+        /// </summary>
+        private string _requete;
 
-//        /// <summary>
-//        /// Méthode de connexion à la base de données.
-//        /// </summary>
-//        public static void OuvrirConnexion()
-//        {
-//            try
-//            {
-//                _connexion = new MySqlConnection(CNX);
-//                if (_connexion.State == ConnectionState.Closed)
-//                {
-//                    _connexion.Open();
-//                }
-//            }
-//            catch
-//            {
-//                throw new Exception("Erreur de connexion à la base de données MySQL.");
-//            }
-//        }
-
-//        /// <summary>
-//        /// Méthode publique de fermeture de la connexion à la base de données MySQL.
-//        /// </summary>
-//        public static void FermerConnexion()
-//        {
-//            if (_connexion.State != ConnectionState.Closed)
-//            {
-//                _connexion.Dispose();
-//                _connexion.Close();
-//            }
-//        }
-
-//        public void Comparer()
-//        {
-
-//        }
-
-<<<<<<< Updated upstream
-                // Définition de la requête SQL
-                MySqlCommand cmd = new MySqlCommand(requete, _connexion);
-                cmd.CommandType = CommandType.Text;
-
-                // Execution de la requête SQL et récuparation du datareader
-                var resultReader = cmd.ExecuteReader();
-
-                // Récupération des valeurs et création d'une liste de FaceId (sous forme de chaine)
-                List<string> lesFaceId = new List<string>();
-                if (resultReader.HasRows)
+        /// <summary>
+        /// Méthode de connexion à la base de données.
+        /// </summary>
+        public static void OuvrirConnexion()
+        {
+            try
+            {
+                _connexion = new MySqlConnection(CNX);
+                if (_connexion.State == ConnectionState.Closed)
                 {
-                    while (resultReader.Read())
-                    {
-                        lesFaceId.Add(resultReader.GetString(0));
-                    }
+                    _connexion.Open();
                 }
+            }
+            catch
+            {
+                throw new Exception("Erreur de connexion à la base de données MySQL.");
+            }
+        }
 
-                // Fermeture de la connexion
+        /// <summary>
+        /// Méthode publique de fermeture de la connexion à la base de données MySQL.
+        /// Va permettre de libérer les ressources de la base de données après une requête.
+        /// </summary>
+        public static void FermerConnexion()
+        {
+            if (_connexion.State != ConnectionState.Closed)
+            {
+                _connexion.Dispose();
+                _connexion.Close();
+            }
+        }
+
+        /// <summary>
+        /// Méthode statique permettant d'exécuter la requete SQL 
+        /// enregistrée dans la variable requete.
+        /// Permet d'éviter la duplication de code.
+        /// </summary>
+        /// <param name="requete">Requête enregistrée.</param>
+        /// <exception cref="Exception">La requête n'a pu aboutir.</exception>
+        //public static void ExecuterRequete(string requete)
+        //{
+        //    try
+        //    {
+        //        MySqlCommand cmd = new MySqlCommand(requete, _connexion);
+        //        cmd.CommandType = CommandType.Text;
+        //        Console.WriteLine("Requête effectuée.");
+        //    }
+        //    catch
+        //    {
+        //        throw new Exception("La requête n'a pu aboutir.");
+        //    }
+        //}
+
+        /// <summary>
+        /// Méthode de récupération de la photo.
+        /// Destinée à l'affichage.
+        /// </summary>
+        public void RecupPhoto()
+        {
+            this._requete = @"SELECT value FROM photos;";
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(this._requete, _connexion);
+                cmd.CommandType = CommandType.Text;
+                var scalar = cmd.ExecuteScalar();  // Permet de stocker le résultat de la requête quand elle renvoie une valeur unitaire.
+                Console.WriteLine("Requête effectuée.");
+                // ExecuterRequete(this._requete);
                 FermerConnexion();
-
-                // On retourne le résultat de la requête
-                return lesFaceId;
             }
             catch
             {
@@ -154,21 +163,3 @@
         }
     }
 }
-=======
-//        public void RecupInfosUser()
-//        {
-//            this._requete = @"SELECT photo, nom, prenom, type, birth, sexe, status, email FROM users INNER JOIN photos ON users.photo = photos.id WHERE faceid = @faceid;";
-//            try
-//            {
-//                MySqlCommand cmd = new MySqlCommand(this._requete, _connexion);
-//                cmd.CommandType = CommandType.Text;
-//                Console.WriteLine("Requête effectuée.");
-//            }
-//            catch
-//            {
-//                throw new Exception("La requête n'a pu aboutir.");
-//            }
-//        }
-//    }
-//}
->>>>>>> Stashed changes
